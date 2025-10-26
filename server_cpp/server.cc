538d3d88055dcc53ed04c719a85bfdc2aa58e199
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2025-10-26 18:18:59 -0300

    Update server.cc

--- server_cpp/server.cc
+++ server_cpp/server.cc
@@ -1,20 +1,27 @@
 #include "file_processor.grpc.pb.h"
+#include "file_processor_service_impl.h"
 #include <grpcpp/grpcpp.h>
 #include <iostream>
 
 using grpc::Server;
 using grpc::ServerBuilder;
 
-extern class FileProcessorServiceImpl;  // Declarado em file_processor_service_impl.cc
-
 void RunServer() {
-  std::string server_address("0.0.0.0:50051");
+  std::string server_address("0.0.0.0:50051"); // bind em todas interfaces
   FileProcessorServiceImpl service;
-  ServerBuilder builder;
+
+  grpc::ServerBuilder builder;
+  // Escutar em todas as interfaces para aceitar conexões locais/externas
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Servidor gRPC ouvindo em " << server_address << std::endl;
+
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  if (!server) {
+    std::cerr << "Falha ao iniciar o servidor gRPC em " << server_address << std::endl;
+    return;
+  }
+  std::cout << "Servidor rodando e escutando em " << server_address << std::endl;
+
   server->Wait();
 }
 
